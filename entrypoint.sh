a04849d15752177567cfd26fa9c91d227741654d
Karel Boek <karel.boek@raskenlund.com>
2022-12-27 13:43:01 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,21 @@
 set -e
 
 # Navigate to the working directory
+echo "Navigating to ${INPUT_WORKINGDIR}"
+
 cd "${INPUT_WORKINGDIR:-.}"
 
 # Find the template file
+echo "Find the template file (${INPUT_TEMPLATEFILE})"
+
 if ( [ ! -f "${INPUT_TEMPLATEFILE}" ] &&  $INPUT_TEMPLATEFILE != *.pkr.hcl ] ]); then
     echo "${INPUT_TEMPLATEFILE} does not exit in the working directory (${INPUT_WORKINGDIR})"
     exit 1
 fi
 
 # Find the var file file and it should be a json file
+echo "Find the var file"
+
 if [[ ! -f "${INPUT_VARFILE}" ]] && [[ $INPUT_VARFILE != *.pkr.hcl ]]; then
     echo "$INPUT_VARFILE not found in the working directory (${INPUT_WORKINGDIR})"
     exit 1
@@ -24,6 +30,7 @@ fi
 
 set +e
 # Run Packer init
+echo "Run Packer init"
 PACKER_INIT_OUTPUT=$(sh -c "packer init ${variableCommand} ${INPUT_TEMPLATEFILE}" 2>&1)
 echo "$PACKER_INIT_OUTPUT"
 echo "Ending running packer init ..."
@@ -31,6 +38,7 @@ set -e
 
 set +e
 # Run Packer validate
+echo "Run Packer validate
 PACKER_VALIDATE_OUTPUT=$(sh -c "packer validate ${variableCommand} ${INPUT_TEMPLATEFILE}" 2>&1)
 echo "$PACKER_VALIDATE_OUTPUT"
 echo "Ending running packer validate ..."
